2cfb6c65cde25315f8685b8a47642407bd882b9d
Bilydesty <35246444+Bilydesty@users.noreply.github.com>
2020-08-25 01:06:34 +0800

    Add files via upload

---
+++ MPU6050.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <wiringPi.h>
+#include <wiringPiI2C.h>
+#include "inv_mpu.h"
+#include "inv_mpu_dmp_motion_driver.h"
+
+int main (void)
+{ 
+	if (wiringPiSetup()==-1) 
+	{
+		printf("wiringPiSetup初始化失败\n");
+		return -1;
+	}
+	if(mpu_dmp_init())
+	{
+		printf("dmp初始化失败\n");
+		return 1;
+	}else{
+		printf("初始化成功\n");
+	}
+	float pitch,roll,yaw;
+
+	while(1){ 
+		while (mpu_dmp_get_data(&pitch, &roll, &yaw) != 0) {}
+		printf("pitch:%f\troll:%f\tyaw:%f\n",pitch,roll,yaw);
+	}
+	return 0 ;
+}
+
+
